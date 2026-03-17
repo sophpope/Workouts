4be0d8bf3d04115bcbf760500ff50a0460bbaf52
Sophie Pope <sophiepope393@gmail.com>
2026-03-17 11:29:52 +0000

    Setting up database conection to Fast Api

---
+++ backend/models.py
@@ -0,0 +1,69 @@
+from sqlmodel import SQLModel, Field 
+from typing import Optional
+from datetime import date, datetime
+from decimal import Decimal
+
+
+class User(SQLModel, table=True):
+    __table_name__="users"
+    user_id: Optional[int] = Field(default=None, primary_key=True)
+    username: str
+    email: str
+    password_hash: str
+    created_at: Optional[datetime] = None
+
+
+class Exercise(SQLModel, table=True):
+    __table_name__="exercises"
+    exercise_id: Optional[int] = Field(default=None, primary_key=True)
+    exercise_name: str
+    primary_muscle: str
+    secondary_muscle: str
+    difficulty: str
+    category: str
+    equipment: str
+    description: str
+    created_at: Optional[datetime] = None
+
+class PersonalRecords(SQLModel, table=True):
+    __table_name__="personal_records"
+    pr_id: Optional[int]= Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="users.user_id")
+    record_type: str
+    record_value: int
+    achieved_on: date
+    exercise_id: int = Field(foreign_key="exercises.exercise_id")
+    measurement_type: str
+    set_id: int = Field(foreign_key="sets.set_id")
+
+class Set(SQLModel, table=True):
+    __table_name__="sets"
+    set_id: Optional[int]= Field(default=None, primary_key=True)
+    workout_exercise_id: int = Field(foreign_key="workout_exercises.workout_exercise_id")
+    set_number: int
+    reps: Optional[int]
+    weight:Optional[Decimal]
+    duration_seconds: Optional[int]
+    distance_meters: Optional[Decimal]
+    notes: Optional[str]
+
+class WorkoutExercise(SQLModel, table=True):
+    __table_name__="workout_exercises"
+    workout_exercise_id: Optional[int]= Field(default=None, primary_key=True)
+    workout_id: int = Field(foreign_key="workouts.workout_id")
+    exercise_id: int = Field(foreign_key="exercises.exercise_id")
+    exercise_order: int
+
+class Workout(SQLModel, table=True):
+    __table_name__="workouts"
+    workout_id: Optional[int]= Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="users.user_id")
+    workout_name: str
+    workout_date: date
+    notes: str
+                                     
+                                     
+
+
+
+
