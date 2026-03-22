01287e2075bd7edd87a0dd67a14b6cc45746ebf6
Sophie Pope <sophiepope393@gmail.com>
2026-03-22 17:17:20 +0000

    updating the models for deb connection to FastAPI

--- backend/models.py
+++ backend/models.py
@@ -63,8 +63,89 @@ class Workout(SQLModel, table=True):
     notes: str
 
 # Models for post requests, adding new data to the database                                     
-                                     
+                                 
+
+class UserCreate(SQLModel):
+    username: str
+    email: str
+    password_hash: str
 
 
+class ExerciseCreate(SQLModel):
+    exercise_name: str
+    primary_muscle: str
+    secondary_muscle: str
+    difficulty: str
+    category: str
+    equipment: str
+    description: str
 
+class PersonalRecordsCreate(SQLModel):
+    user_id: int = Field(foreign_key="users.user_id")
+    record_type: str
+    record_value: int
+    achieved_on: date
+    exercise_id: int = Field(foreign_key="exercises.exercise_id")
+    measurement_type: str
+    set_id: int = Field(foreign_key="sets.set_id")
 
+class SetCreate(SQLModel):
+    workout_exercise_id: int = Field(foreign_key="workout_exercises.workout_exercise_id")
+    set_number: int
+    reps: Optional[int]
+    weight:Optional[Decimal]
+    duration_seconds: Optional[int]
+    distance_meters: Optional[Decimal]
+    notes: Optional[str]
+
+class WorkoutExerciseCreate(SQLModel):
+    workout_exercise_id: Optional[int]= Field(default=None, primary_key=True)
+    workout_id: int = Field(foreign_key="workouts.workout_id")
+    exercise_id: int = Field(foreign_key="exercises.exercise_id")
+    exercise_order: int
+
+class WorkoutCreate(SQLModel):
+    user_id: int = Field(foreign_key="users.user_id")
+    workout_name: str
+    workout_date: date
+    notes: str
+
+# Public models to be used as a response model - showing what our API is returning
+
+class UserPublic(SQLModel):
+    username: str
+
+class ExercisePublic(SQLModel):
+    exercise_id: int
+    exercise_name: str
+    primary_muscle: str
+    secondary_muscle: str
+    difficulty: str
+    category: str
+    equipment: str
+    description: str
+    #created_at: datetime
+
+class PersonalRecordsPublic(SQLModel):
+    record_type: str
+    record_value: int
+    achieved_on: date
+    measurement_type: str
+
+class SetPublic(SQLModel):
+    set_number: int
+    reps: int
+    weight: Decimal
+    duration_seconds: int
+    distance_meters: Decimal
+    notes: str
+
+class WorkoutExercisePublic(SQLModel):
+    workout_exercise_id: int
+    workout_id: int 
+    exercise_order: int    
+
+class WorkoutPublic(SQLModel):
+    workout_name: str
+    workout_date: date
+    notes: str
