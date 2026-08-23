901728d6bfb82c56b9783ce80965722e8f74ee08
Sophie Pope <sophiepope393@gmail.com>
2026-08-23 08:44:43 +0100

    setting up access token function

--- backend/auth.py
+++ backend/auth.py
@@ -9,3 +9,9 @@ password_hash = PasswordHash.recommended()
 # setting up login functions 
 def verify_password(plain_password: str, hashed_password: str):
     return password_hash.verify(plain_password, hashed_password)
+
+def create_access_token(user_id: int):
+    expiration = datetime.now(timezone.utc) + timedelta(minutes=30)
+    payload = {"user_id": str(user_id), "exp": expiration}
+    token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+    return token
