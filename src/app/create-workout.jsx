5841fa499a844fbca64e11336a92ece6a320b1a9
Sophie Pope <sophiepope393@gmail.com>
2026-09-01 15:44:43 +0100

    adding functionaility for adding workouts

--- src/app/create-workout.jsx
+++ src/app/create-workout.jsx
@@ -1,12 +1,56 @@
 import { View, Text, TextInput, Pressable } from 'react-native';
 import { useState } from 'react';
 import styles from '../styles/formStyles.js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useRouter } from 'expo-router';
+import api from '../api.js';
+
 
 export default function CreateWorkout() {
     const [workoutName, setWorkoutName] = useState('');
     const [workoutDate, setWorkoutDate] = useState('');
     const [notes, setNotes] = useState('');
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
 
+    const router = useRouter();
+
+    const handleCreateWorkout = async () => {
+        if (!workoutName.trim()) {
+            setError("Please enter a workout name");
+            return;
+        } else if (!workoutDate.trim()) {
+            setError("Please enter a workout date");
+            return;
+        }
+
+        setError('');
+
+        try {
+            const token = await AsyncStorage.getItem('access_token');
+            if (token) {
+                api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+            }
+
+
+            const response = await api.post('/create_workout', {
+                workout_name: workoutName,
+                workout_date: workoutDate,
+                notes: notes
+            });
+
+            setSuccess(`Workout ${response.data.workout_name} created successfully`);
+
+            setWorkoutName('');
+            setWorkoutDate('');
+            setNotes('');
+
+            router.replace('/profile');
+
+        } catch (error) {
+            setError(error.response?.data?.detail || 'Error creating workout. Please check your details and try again.');
+        }
+    }
     return (
         <View>
             <Text style={styles.title}>Create Workout</Text>
@@ -35,7 +79,11 @@ export default function CreateWorkout() {
                 onChangeText={setNotes}
             />
 
-            <Pressable>
+            {error && <Text style={styles.error}>{error}</Text>}
+
+            {success && <Text style={styles.success}>{success}</Text>}
+
+            <Pressable onPress={handleCreateWorkout}>
                 <Text style={styles.workoutButton}>Create Workout</Text>
             </Pressable>
         </View>
